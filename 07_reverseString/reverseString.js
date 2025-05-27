512915e5e961f8aca47b6fea7e75ae86c40bfff7
Tejaswi Manoj <tejaswimanoj@gmail.com>
2025-05-26 20:39:01 -0400

    Implement my solution for Exercise 7

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let arrStr = string.split("");
+    let revArr = [];
+    for (let i=arrStr.length-1;i>=0;i--){
+        revArr.push(arrStr[i]);
+    }
+    return revArr.join("")
 };
+
+// reversed = reverseString("hello");
+// console.log(reversed);
 
 // Do not edit below this line
 module.exports = reverseString;
