ddfc77cf8daf2ad4c0df4021401269177f4199bc
Tejaswi Manoj <tejaswimanoj@gmail.com>
2025-05-27 15:26:20 -0400

    Implement my solution for Exercise 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let total = 0;
+    let first, last;
+    if (a>b){
+        first = b;
+        last = a;
+    } else{
+        last = b;
+        first = a;
+    }
 
+    if (!(Number.isInteger(first))||!(Number.isInteger(last))||first<0||last<0){
+        return "ERROR";
+    }
+
+    for (let i=first; i<=last; i++){
+        total += i;
+    }
+    return total;
 };
+
+
+// console.log(sumAll(1,5));
 
 // Do not edit below this line
 module.exports = sumAll;
