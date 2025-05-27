a6a50e2099a8ae36468a1d35998d4883f5247b23
Tejaswi Manoj <tejaswimanoj@gmail.com>
2025-05-26 20:23:05 -0400

    Implement my solution for Exercise 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, number) {
 
+    if (number<0){
+        return "ERROR";
+    }
+    let newStr = "";
+    for (let i=0; i<number; i++){
+        newStr += string;
+    }
+    return newStr;
 };
 
 // Do not edit below this line
